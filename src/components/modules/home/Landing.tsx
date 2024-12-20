a3b7e7ff37cc05a1123fdebdb2d8964584fc154c
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-12-20 12:57:57 +0600

    set up basic landing page

---
+++ src/components/modules/home/Landing.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@nextui-org/input";
+
+const LandingPage = () => {
+    return (
+        <section className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
+        <Input />
+       </section>
+    );
+};
+
+export default LandingPage;
---
+++ src/components/modules/home/RecentPosts.tsx
@@ -0,0 +1,9 @@
+const RecentPosts = () => {
+    return (
+        <div>
+            <h1>recent posts</h1>
+        </div>
+    );
+};
+
+export default RecentPosts;
