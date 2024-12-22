4c126e82bbc430860b233b0f187fd68390557934
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-12-23 02:39:11 +0600

    added a dropdown bar for admin and user role

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -12,14 +12,15 @@ import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
+import NavbarDropdown from "./NavbarDropdown";
+import { ThemeSwitch } from "./theme-switch";
+
 import { siteConfig } from "@/src/config/site";
-import { ThemeSwitch } from "@/src/components/ui/theme-switch";
-import {
-  Logo,
-} from "@/src/components/icons";
+import { Logo } from "@/src/components/icons";
+
+
 
 export const Navbar = () => {
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -35,7 +36,7 @@ export const Navbar = () => {
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
                 )}
                 color="foreground"
                 href={item.href}
@@ -54,6 +55,9 @@ export const Navbar = () => {
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
         </NavbarItem>
+        <NavbarItem className="hidden sm:flex gap-2">
+          <NavbarDropdown />
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -70,8 +74,8 @@ export const Navbar = () => {
                   index === 2
                     ? "primary"
                     : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+                    ? "danger"
+                    : "foreground"
                 }
                 href="#"
                 size="lg"
